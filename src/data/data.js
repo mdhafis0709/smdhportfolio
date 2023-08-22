abb592b5dcefd96a5e371cc87003b27615185338
mdhafis0709 <89003088+mdhafis0709@users.noreply.github.com>
2023-08-22 09:53:31 +0530

    Add files via upload

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@
 export const data = [
   {
     id: 1,
-    img: 'https://i.postimg.cc/7YwcY1mG/Screenshot-61.png',
+    img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
     title: "college website",
     desc: "",
     link: "http://hit.edu.in/it/",
